3e4603a963da9030e977e587c1a3d04c53324e7e
Michael McClain <Mikemcclain14@gmail.com>
2022-10-26 14:44:46 +0200

    Video 7.78 finished

--- Argon Assault/Assets/Scripts/PlayerController.cs
+++ Argon Assault/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] float xStrafeSpeed = 10f;
     [SerializeField] float yStrafeSpeed = 10f;
+    [SerializeField] float xRange = 10f;
+    [SerializeField] float yRange = 10f;
     [SerializeField] InputAction movement;
 
     void Start()
@@ -30,12 +32,14 @@ public class PlayerController : MonoBehaviour
         float yStrafe = movement.ReadValue<Vector2>().y;
 
         float xOffset = xStrafe * Time.deltaTime * xStrafeSpeed;
-        float newXPos = transform.localPosition.x + xOffset;
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
 
         float yOffset = yStrafe * Time.deltaTime * yStrafeSpeed;
-        float newYPos = transform.localPosition.y + yOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
 
-        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
 
     }
 }
