36da51c429e11fbd4b5973647842f9f520c15e0f
Verdante3D <Mikemcclain14@gmail.com>
2022-11-18 13:57:46 +0100

    Video 4.92 finished

--- Argon Assault/Assets/Scripts/CollisionHandler.cs
+++ Argon Assault/Assets/Scripts/CollisionHandler.cs
@@ -8,16 +8,21 @@ public class CollisionHandler : MonoBehaviour
 {
     [SerializeField] float loadDelay = 1f;
 
+    [SerializeField] ParticleSystem crashVFX;
+
     void OnTriggerEnter(Collider other)
     {
         StartCrashSequence();
-            }
+    }
 
     private void StartCrashSequence()
     {
+        GetComponent<MeshRenderer>().enabled = false;
         GetComponent<PlayerController>().enabled = false;
+        GetComponent<BoxCollider>().enabled = false;
         Invoke("ReloadLevel", loadDelay);
-           }
+        crashVFX.Play();
+    }
 
     void ReloadLevel()
     {
