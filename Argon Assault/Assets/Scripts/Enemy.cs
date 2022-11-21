8cdb55604ac1cd995f8ffc10a79e10c738db73de
Verdante3D <Mikemcclain14@gmail.com>
2022-11-21 13:07:09 +0100

    Video 4.97 finished

--- Argon Assault/Assets/Scripts/Enemy.cs
+++ Argon Assault/Assets/Scripts/Enemy.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
+    [SerializeField] GameObject hitVFX;
     [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitpoints = 2;
 
     Scoreboard scoreBoard;
 
@@ -18,11 +20,20 @@ public class Enemy : MonoBehaviour
     void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+
+        if (hitpoints < 1)
+        {
+            KillEnemy();
+        }
     }
     void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity);
+        vfx.transform.parent = parent;
+        hitpoints--;
+
         scoreBoard.IncreaseScore(scorePerHit);
+        Debug.Log($"Hitpoints left: {hitpoints}");
     }
 
     void KillEnemy()
